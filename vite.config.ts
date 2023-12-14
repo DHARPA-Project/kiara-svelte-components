7f5d1720bb06612c5e2b0f6be1a521800115e0f8
Caro Appleby <caroline.appleby@cantab.net>
2023-12-14 17:18:04 +0000

    Update vitest version, add coverage reporting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: ['./src/lib/test/setup.ts'],
+		coverage: {
+			reporter: ['text', 'html'],
+			include: ['src/lib/**']
+		}
 	}
 });
